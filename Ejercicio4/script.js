d51f8a214607e9a6176ac0396780fa612d440fa7
JSToledo-359 <toledojr996@gmail.com>
2025-09-26 01:10:54 -0300

    Ejercicio4

---
+++ Ejercicio4/script.js
@@ -0,0 +1,3 @@
+/*Realizar un cuento interactivo al estilo “Elije tu Propia Aventura”. Al final de cada
+párrafo el lector podrá elegir entre 2 o más opciones. El cuento podrá tener 1 o más
+finales.*/
